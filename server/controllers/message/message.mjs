710f6c143ac5c9c09f92da4a947e5b765a8eeddf
Zinedine <megnouche.z@gmail.com>
2022-04-18 15:10:34 +0200

    Ajout de message et state

---
+++ server/controllers/message/message.mjs
@@ -0,0 +1,46 @@
+import pg from 'pg'
+import dotenv from 'dotenv'
+
+dotenv.config()
+var url = process.env.URL
+var client = new pg.Client(url);
+
+/**
+ * Cette fonction permet de lire les informations d'un contact
+ * @param id
+ */
+const loadMessageByid = (id) => {
+  client.connect(function (err) {
+    if (err) {
+      return console.error('could not connect to postgres', err)
+    }
+    client.query(`select id, object, content, senddate, sentdate, sendhour, senthour, idstate, idlist, idmodel from message where id = ${id}`, function (err, result) {
+      if (err) {
+        return console.error('error running query', err)
+      }
+      console.log(result.rows[0])
+      const res = result.rows[0]
+      client.end()
+      return res
+    })
+  })
+}
+const deleteMessage = (id) => {
+  client.connect(function (err) {
+    if (err) {
+      return console.error('could not connect to postgres', err)
+    }
+    client.query(`delete from message where id = ${id}`, function (err, result) {
+      if (err) {
+        return console.error('error running query', err)
+      }
+      console.log(result.rows[0])
+      console.log(`Message ${id} a bien été supprimé`)
+    })
+  })
+}
+
+
+
+
+export { loadMessageByid, deleteMessage }
--- server/routes.mjs
+++ server/routes.mjs
@@ -1,19 +1,41 @@
 import { loadContact } from "./controllers/contact/contact.mjs"
+import { loadMessageByid, deleteMessage } from "./controllers/message/message.mjs"
+
 import express from 'express'
 
 const router = express.Router()
 
 /**
- * 
+ *
  */
 router.get('/contact/:id', (req, res) => {
-    try {
-        const id = req.params.id
-        res.send(loadContact(id))
-    } catch (error) {
-        res.send(404)
-        console.log("error")
-    }
+  try {
+    const id = req.params.id
+    res.send(loadContact(id))
+  } catch (error) {
+    res.send(404)
+    console.log("error")
+  }
+})
+
+router.get('/message/:id', (req, res) => {
+  try {
+    const id = req.params.id
+    res.send(loadMessageByid(id))
+  } catch (error) {
+    res.send(404)
+    console.log("error")
+  }
+})
+
+router.delete('/message/:id', (req, res) => {
+  try {
+    const id = req.params.id
+    res.send(deleteMessage(id))
+  } catch (error) {
+    res.send(404)
+    console.log(`Il n'existe pas de message avec l'id : ${id}`)
+  }
 })
 
 export default router
