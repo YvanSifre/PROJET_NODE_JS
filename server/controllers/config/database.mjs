4976e8549d8be9131869b468a4d3bf19275d4b44
thomas southasa <thomas.southasa@gmail.com>
2022-04-20 19:48:47 +0200

    update connexion bdd

---
+++ server/controllers/config/database.mjs
@@ -0,0 +1,16 @@
+import pg from 'pg'
+import dotenv from 'dotenv'
+
+dotenv.config()
+var url = process.env.URL
+const client = new pg.Pool({
+    connectionString: url,
+});
+
+client.connect(function (err) {
+    if (err) {
+        return console.error('could not connect to postgres', err)
+    }
+})
+
+export { client }
--- server/controllers/model-template/model-template.mjs
+++ server/controllers/model-template/model-template.mjs
@@ -1,91 +1,62 @@
-import pg from 'pg'
-import dotenv from 'dotenv'
-
-dotenv.config()
-var url = process.env.URL
-var client = new pg.Client(url);
+import { client } from '../config/database.mjs'
 
 /**
  * Cette fonction permet de lire les informations d'un contact
  * @param id 
  */
 const loadTemplateById = (id) => {
-    client.connect(function (err) {
+
+    client.query(`select id, name, filename from model where id = ${id}`, function (err, result) {
         if (err) {
-            return console.error('could not connect to postgres', err)
+            return console.error('error running query', err)
         }
-        client.query(`select id, name, filename from model where id = ${id}`, function (err, result) {
-            if (err) {
-                return console.error('error running query', err)
-            }
-            console.log(result.rows[0])
-            const res = result.rows[0]
-            client.end()
-            return res
-        })
+        console.log(result.rows[0])
+        const res = result.rows[0]
+        return res
     })
+
 }
 
 const loadTemplate = () => {
-    client.connect(function (err) {
+
+    client.query(`select id, name, filename from model`, function (err, result) {
         if (err) {
-            return console.error('could not connect to postgres', err)
+            return console.error('error running query', err)
         }
-        client.query(`select id, name, filename from model`, function (err, result) {
-            if (err) {
-                return console.error('error running query', err)
-            }
-            if (result.rowCount == 0) {
-                return console.error(`Pas de message`)
-            }
-            console.log(result.rows)
-            const res = result.rows
-            client.end()
-            return res
-        })
+        if (result.rowCount == 0) {
+            return console.error(`Pas de message`)
+        }
+        console.log(result.rows)
+        const res = result.rows
+        return res
     })
 }
 
 const deleteTemplate = (id) => {
-    client.connect(function (err) {
+    client.query(`delete from model where id = ${id}`, function (err, result) {
         if (err) {
-            return console.error('could not connect to postgres', err)
+            return console.error('error running query', err)
         }
-        client.query(`delete from model where id = ${id}`, function (err, result) {
-            if (err) {
-                return console.error('error running query', err)
-            }
-            console.log(result.rows[0])
-            console.log(`Template: ${id} a bien été supprimé`)
-        })
+        console.log(result.rows[0])
+        console.log(`Template: ${id} a bien été supprimé`)
     })
 }
 
 const addTemplate = (name, filename) => {
-    client.connect(function (err) {
+    client.query(`insert into model values (DEFAULT, '${name}', '${filename}')`, function (err, result) {
         if (err) {
-            return console.error('could not connect to postgres', err)
+            return console.error('error running query test', err)
         }
-        client.query(`insert into model values (DEFAULT, '${name}', '${filename}')`, function (err, result) {
-            if (err) {
-                return console.error('error running query test', err)
-            }
-            console.log(`Template: ${name} a bien été ajouté`)
-        })
+        console.log(`Template: ${name} a bien été ajouté`)
     })
 }
 
 const updateTemplate = (id, name) => {
-    client.connect(function (err) {
+    client.query(`update model set name = '${name}' where id = ${id}`, function (err, result) {
         if (err) {
-            return console.error('could not connect to postgres', err)
+            return console.error('error running query', err)
         }
-        client.query(`update model set name = '${name}' where id = ${id}`, function (err, result) {
-            if (err) {
-                return console.error('error running query', err)
-            }
-            console.log(`Template: ${name} a bien été modifié`)
-        })
+        console.log(`Template: ${name} a bien été modifié`)
     })
 }
 
